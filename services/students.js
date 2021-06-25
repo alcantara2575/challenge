a062cc75c4caa8f71c54a490b4b4ad4eab0b0249
alcantara2575 <alcantara2575@gmail.com>
2021-06-25 16:23:24 -0400

    feat: :sparkles: Add elements to table

--- services/students.js
+++ services/students.js
@@ -6,8 +6,7 @@ async function getMultiple(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
     `SELECT * 
-    FROM students`, 
-    [offset, config.listPerPage]
+    FROM students LIMIT `+ offset + `,` + config.listPerPage
   );
   const data = helper.emptyOrRows(rows);
   const meta = {page};
@@ -18,6 +17,25 @@ async function getMultiple(page = 1){
   }
 }
 
-module.exports = {
-  getMultiple
+async function create(data){
+    const result = await db.query(
+      `INSERT INTO students (firstName, lastName, email, age, grade) 
+      VALUES 
+      ("`+ data.firstName + `", "` + data.lastName + `", "` +
+        data.email + `", ` +
+        data.age + `, ` +
+        data.grade +`)`, 
+    );
+  
+    let message = 'Error in creating students data';
+  
+    if (result.affectedRows) {
+      message = 'Programming language created successfully';
+    }
+  return {message};
 }
+  
+module.exports = {
+    getMultiple,
+    create
+  }
